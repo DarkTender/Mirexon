b06ece698097b65fb2d77288185344582be8260b
DarkTender <82323669+DarkTender@users.noreply.github.com>
2025-04-20 04:36:09 +0200

    Delete mirexon directory

--- mirexon/pages/api/credits.js
+++
@@ -1,7 +0,0 @@
-import { getCredits } from "../../lib/user";
-
-export default async function handler(req, res) {
-  const email = req.query.email;
-  const credits = await getCredits(email);
-  res.status(200).json({ credits });
-}
